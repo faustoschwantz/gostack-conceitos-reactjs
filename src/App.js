ac6332b92c0e21b57ca38013eb7e2e7de08548a2
Fausto Schwantz <faustoschwantz@gmail.com>
2020-08-31 22:42:22 -0300

    Repository list, delete and add

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import "./styles.css";
+import api from "./services/api";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+  useEffect(() => {
+    api.get("repositories").then(({ data }) => {
+      setRepositories(data);
+    });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post("repositories", {
+      title: "gostack-conceitos-reactjs",
+      url: "https://github.com/faustoschwantz/gostack-conceitos-reactjs",
+      techs: ["Javascript", "ReactJs"],
+    });
+
+    setRepositories([...repositories, response.data]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete("repositories/" + id);
+    setRepositories(repositories.filter((x) => x.id !== id));
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map(({ id, title }) => (
+          <li key={id}>
+            {title}
+            <button onClick={() => handleRemoveRepository(id)}>Remover</button>
+          </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
